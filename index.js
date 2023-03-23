1c663d57ba19233a6865b614f382326a65f6db06
Dmytro Kraiovyi <dkraiovyi@Dmytros-MacBook-Pro.local>
2023-03-23 21:03:11 +0100

    firstcommit

---
+++ index.js
@@ -0,0 +1,19 @@
+// proofForValera.js
+
+function valeraInstalled() {
+  return 'I am function Valera. You installed me.';
+}
+
+function youAreStupid() {
+  return 'You are so stupid. I was right.';
+}
+
+function sasaiLalka() {
+  return 'sasai lalka';
+}
+
+module.exports = {
+  valeraInstalled,
+  youAreStupid,
+  sasaiLalka,
+};
